c73fd4b3d9badfa716de838c5443eff8766766bb
Martin Mareš <mares@datalite.cz>
2021-07-16 22:59:26 +0200

    Exercise A - variables

---
+++ exercise/a_variables/variables/src/main.rs
@@ -0,0 +1,11 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 1;
+
+fn main() {
+
+    let (missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+    // let a = 1;
+    
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    println!("{} missiles left", missiles - ready);
+}
